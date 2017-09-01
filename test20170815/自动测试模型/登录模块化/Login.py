594139370bd93a0026a765ced4ef45b4ff069830
Woody-1018 <wuqi19901018@aliyun.com>
2017-09-01 15:48:19 +0800

    20170825

--- test20170815/自动测试模型/登录模块化/Login.py
+++ test20170815/自动测试模型/登录模块化/Login.py
@@ -1,17 +1,18 @@
+
 class UserLogin(object):
-    def login(self):
+
+    @staticmethod
+    def login(driver):
         driver.find_element_by_name("account").clear()
         driver.find_element_by_name("account").send_keys("admin")
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("WuQi19901018")
         driver.find_element_by_id("submit").click()
 
-    def logout(self):
+    @staticmethod
+    def logout(driver):
         driver.find_element_by_xpath(".//*[@id='topnav']/a[1]").click()
         driver.quit()
-
-
-
 
 
 
--- test20170815/自动测试模型/登录模块化/TestCase01.py
+++ test20170815/自动测试模型/登录模块化/TestCase01.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
 from Login import UserLogin
+import time
 
 driver = webdriver.Chrome()
+driver.get("http://localhost/zentaopms/www/")
+driver.maximize_window()
+UserLogin.login(driver)
+time.sleep(3)
+UserLogin.logout(driver)
+
